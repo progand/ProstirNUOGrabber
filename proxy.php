c34e925163a2aa073c502dcaf672ef1a39880478
progand <lach@ukr.net>
2013-08-03 14:09:54 +0300

    [*] Додав проксі

--- proxy.php
+++ proxy.php
@@ -1,5 +1,8 @@
 <?php
 $url = $_REQUEST['url'];
-if (preg_match('/\b(https?|ftp):\/\/*/', $url) !== 1) die;
+if (preg_match('/\b(https?|ftp):\/\/*/', $url) !== 1) {
+    echo '<h1>No-no-no!!!</h1>';
+    die;
+}
 echo (file_get_contents($url));
 ?>
